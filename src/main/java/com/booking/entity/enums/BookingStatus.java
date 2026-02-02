b757e5dda03d0f5a55d8ea2e795fe000f3306580
gajendra-ingle <gajendraingle01@gmail.com>
2026-02-02 10:51:41 +0530

    Moved enum classes to enums package

---
+++ src/main/java/com/booking/entity/enums/BookingStatus.java
@@ -0,0 +1,6 @@
+package com.booking.entity.enums;
+
+public enum BookingStatus {
+    ACTIVE,
+    CANCELLED
+}
---
+++ src/main/java/com/booking/entity/enums/SlotStatus.java
@@ -0,0 +1,6 @@
+package com.booking.entity.enums;
+
+public enum SlotStatus {
+    AVAILABLE,
+    BOOKED
+}
